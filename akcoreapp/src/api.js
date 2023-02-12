7da191c8aefab2b1475f62cbc6a2c4d4b1d571d7
niklasfranz <nf.app@icloud.com>
2023-02-12 03:01:09 +0100

    migrating to public api endpoint

--- akcoreapp/src/api.js
+++ akcoreapp/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: 'http://141.89.39.93/api/',
     json: true
 });
 
--- akcoreapp/vue.config.js
+++ akcoreapp/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    https:true
+    https:false
   },
   publicPath: process.env.NODE_ENV === 'production'
   ? '/test/'
