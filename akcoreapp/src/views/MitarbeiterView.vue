da26d81491fd155aa8eea47b08ec6f334835c01a
Leon Krüger <73723974+lemotho@users.noreply.github.com>
2023-03-09 12:18:33 +0100

    put create mitarbeiter to top of page

--- akcoreapp/src/views/MitarbeiterView.vue
+++ akcoreapp/src/views/MitarbeiterView.vue
@@ -3,6 +3,59 @@
   <LoadingComponent v-if="loading" :loading="this.loading" />
   <ErrorComponent v-if="error" :error="this.error" />
   <div v-if="!error" class="mt-3">
+    <form v-if="!loading && abteilungen.length" class="needs-validation g-3" novalidate>
+        <h5>Mitarbeiter hinzufügen:</h5>
+        <div class="row">
+          <div class="col">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">Name</span>
+              <input type="text" class="form-control" v-model="model.mitarbeiter.mitarbeiterName" placeholder="Name" >
+            </div>
+          </div>
+          <div class="col">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">Email</span>
+              <input type="text" class="form-control" v-model="model.mitarbeiter.mitarbeiterEmail" placeholder="E-Mail" required>
+            </div>
+          </div>
+          <div class="col-3">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">Abteilung</span>
+              <select class="form-control" v-model="model.mitarbeiter.abteilungId" >
+                <option disabled value=undefined>Bitte auswählen</option>
+                <option v-for="abt in abteilungen" :key="abt" v-bind:value="'' + abt.abteilungId">{{ abt.abteilungName }}</option>
+              </select> 
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="btn-container">
+              <button class="btn btn-outline-success" style="width:100%;" @click.prevent="createMitarbeiter" type="submit">Create</button>
+            </div>
+          </div>
+        </div>
+      </form>
+
+      <form v-if="!loading" class="needs-validation g-3" novalidate>
+        <h5>Abteilung hinzufügen:</h5>
+        <div class="row">
+          <div class="col">
+            <div class="input-group mb-3">
+              <span class="input-group-text" id="basic-addon1">Abteilungsname</span>
+              <input type="text" class="form-control" v-model="model.abteilung.abteilungName" placeholder="Name" >
+            </div>
+          </div>
+          <div class="col-2">
+            <div class="btn-container">
+              <button class="btn btn-outline-success" style="width:100%;" @click.prevent="createAbteilung" type="submit">Create</button>
+            </div>
+          </div>
+        </div>
+        <hr class="hr" style="border: 0;
+          height: 3px;
+          background: #095484;
+          background-image: linear-gradient(to right, #ccc, #095484, #ccc);"/>
+      </form>
+
     <form v-if="!loading">
       <ul v-for="abt in abteilungen" :key="abt" class="mb-4 ">
         <div>Abteilung:</div>
@@ -38,60 +91,11 @@
           </div>
         </li>
         <button v-if="!abt.mitarbeiters.length" class="btn btn-outline-danger mt-2" @click.prevent="deleteAbteilung(abt)">Abteilung löschen</button>
-        <hr class="hr" />
+        <hr class="hr" style="hr1"/>
       </ul>
       <div v-if="!abteilungen.length">
         <p class="color-secondary">Es existieren keine Abteilungen. Um einen Mitarbeiter zu anzulegen, legen sie bitte zunächst eine Abteilung an.</p>
         <hr class="hr" />
-      </div>
-    </form>
-
-    <form v-if="!loading && abteilungen.length" class="needs-validation g-3" novalidate>
-      <h5>Mitarbeiter hinzufügen:</h5>
-      <div class="row">
-        <div class="col">
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Name</span>
-            <input type="text" class="form-control" v-model="model.mitarbeiter.mitarbeiterName" placeholder="Name" >
-          </div>
-        </div>
-        <div class="col">
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Email</span>
-            <input type="text" class="form-control" v-model="model.mitarbeiter.mitarbeiterEmail" placeholder="E-Mail" required>
-          </div>
-        </div>
-        <div class="col-3">
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Abteilung</span>
-            <select class="form-control" v-model="model.mitarbeiter.abteilungId" >
-              <option disabled value=undefined>Bitte auswählen</option>
-              <option v-for="abt in abteilungen" :key="abt" v-bind:value="'' + abt.abteilungId">{{ abt.abteilungName }}</option>
-            </select> 
-          </div>
-        </div>
-        <div class="col-2">
-          <div class="btn-container">
-            <button class="btn btn-outline-success" style="width:100%;" @click.prevent="createMitarbeiter" type="submit">Create</button>
-          </div>
-        </div>
-      </div>
-    </form>
-
-    <form v-if="!loading" class="needs-validation g-3" novalidate>
-      <h5>Abteilung hinzufügen:</h5>
-      <div class="row">
-        <div class="col">
-          <div class="input-group mb-3">
-            <span class="input-group-text" id="basic-addon1">Abteilungsname</span>
-            <input type="text" class="form-control" v-model="model.abteilung.abteilungName" placeholder="Name" >
-          </div>
-        </div>
-        <div class="col-2">
-          <div class="btn-container">
-            <button class="btn btn-outline-success" style="width:100%;" @click.prevent="createAbteilung" type="submit">Create</button>
-          </div>
-        </div>
       </div>
     </form>
     <!-- create-new-mitarbeiter row -->
